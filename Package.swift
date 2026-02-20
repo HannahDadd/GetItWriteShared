1038262e5b655ce0e8ccaa58c45529a96f338e03
Hannah Dadd <hannahbdadd@googlemail.com>
2026-02-20 14:00:47 +0000

    package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "GetItWriteShared",
-            dependencies: ["GlobalVariables"]
+            dependencies: ["GlobalVariables"],
+            path: "sources"
         ),
 
     ]
